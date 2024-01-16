e77410bbd8f959912f04612ccd2f9863e3edd68c
pa810p <pa810p@gmail.com>
2024-01-16 08:13:14 +0100

    created product add component

--- adwebstart-webapp/src/App.tsx
+++ adwebstart-webapp/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import { Products } from './components/products';
 
 export const App = () => {
   return (
-    <div>
-      <img src="logo512.png" alt="adwebstart"/>
-    </div>
+      <div>
+          <Products/>
+      </div>
   )  
 }
 
