1a0b8359f98bc9a4b8141587769cf31b541c9bb4
pa810p <pa810p@gmail.com>
2024-08-31 15:49:30 +0200

    swagger support and upgrade dependencies

--- adwebstart-server/src/main/java/net/adfinem/adwebstart/infrastructure/repository/ProductCrudRepository.java
+++ adwebstart-server/src/main/java/net/adfinem/adwebstart/infrastructure/repository/ProductCrudRepository.java
@@ -1,9 +1,18 @@
+/**
+ * AdWebStart - A snippet that can be used to create a web project with
+ * SpringBoot, Postgresql, React
+ * Author:     Paweł Prokop (pa810p@gmail.com)
+ * Repository: https://github.com/pa810p/adwebstart
+ * License:    GNU General Public License v3.0  see: LICENSE
+ */
 package net.adfinem.adwebstart.infrastructure.repository;
 
 import net.adfinem.adwebstart.domain.entity.Product;
 import net.adfinem.adwebstart.domain.repository.ProductRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductCrudRepository extends ProductRepository, CrudRepository<Product, Long> {
 
 }
